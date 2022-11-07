cd3ee602adb60eb6e54695ee02db2a1d9bc4b868
Ben Greanya <bengreanya@gmail.com>
2022-11-07 14:24:01 -0800

    added addFriendButter lister. no functionality.

--- app.js
+++ app.js
@@ -42,25 +42,34 @@ addMushroomButton.addEventListener('click', () => {
 
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
+    const name = friendInputEl.value;
     // create a new friend object
+    const newFriend = {
+        name: newFriends,
+        satisfation: 1,
+    };
+    friendData.push(newFriend);
     // push it into the friends state array, passed in as an argument
+    friendInputEl.value = '';
     // reset the input
     // display all the friends (use a function here)
+    // displayFriends();
+    displayFriends();
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsEl.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
+        const renderFriend(friend);
+        friendListEl.append(friendEl);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
         //             increment the friends satisfaction and decrement your mushrooms
         //             then display your friends and mushrooms with the updated state
-
         // append the friendEl to the friends list in DOM
     }
 }
