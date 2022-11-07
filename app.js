e88840bd8a9abe06058b9dfd0774aa516dd566db
Ben Greanya <bengreanya@gmail.com>
2022-11-07 15:25:17 -0800

    friend event listener functionality working

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ addMushroomButton.addEventListener('click', () => {
 
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
-    const name = friendInputEl.value;
+    const friendName = friendInputEl.value;
     // create a new friend object
     const newFriend = {
-        name: newFriends,
+        name: friendName,
         satisfation: 1,
     };
     friendData.push(newFriend);
@@ -63,20 +63,33 @@ function displayFriends() {
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-        const renderFriend(friend);
-        friendListEl.append(friendEl);
+        const friendEl = renderFriend(friend);
         // this is a clickable list, so . . .
         //     add an event listener to each friend
+        // friendEl.addEventListener('click', () => {
+        //     if (satisfaction < 3 ? mushroomCount > 0: )
+        // });
+        friendEl.addEventListener('click', () => {
+            if (mushroomCount > 0 && friend.satisfaction < 3) {
+                friend.satisfaction++;
+                mushroomCount--;
+            }
+            displayFriends();
+            displayMushrooms();
+        });
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
+
         //             increment the friends satisfaction and decrement your mushrooms
+
         //             then display your friends and mushrooms with the updated state
         // append the friendEl to the friends list in DOM
+        friendsEl.append(friendEl);
     }
 }
 
 function displayMushrooms() {
     // clear out the mushroom div
-
+    mushroomsEl = 
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
     }
